d4d95593d554d94a845e7c79c393d4765b0ac029
solidreno <solidreno@gmail.com>
2024-11-13 13:02:46 +0100

    build

--- src/postbuildCommands.sh
+++ src/postbuildCommands.sh
@@ -1,2 +1,2 @@
-sed -i 'css' 's#/static#/build/static#g' ./build/static/css/*.css && rm ./build/static/css/*.csscss
-sed -i 'html' 's#href="/#href="/build/#g' ./build/index.html && rm./build/*.htmlhtml
+sed -i 'css' 's#/static#/build/static#g' build/static/css/*.css && rm build/static/css/*.csscss
+sed -i 'html' 's#="/#="/build/#g' build/index.html && rm build/*.htmlhtml
